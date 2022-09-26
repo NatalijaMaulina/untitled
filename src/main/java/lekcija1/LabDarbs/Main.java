22804611c54240a54c8873e816a33e2bb079f1b1
NatalijaMaulina <112900467+NatalijaMaulina@users.noreply.github.com>
2022-09-26 21:37:26 +0300

    Initial commit

---
+++ src/main/java/lekcija1/LabDarbs/Main.java
@@ -0,0 +1,10 @@
+package lekcija1.LabDarbs;
+
+public class Main {
+    public static void main(String[] args) {
+         int vecums = 31;
+        System.out.println("Mans vecums ir - "+ vecums);
+
+
+    }
+}
