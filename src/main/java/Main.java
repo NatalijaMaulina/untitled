63cb9ac9b4d3d4c0be6612a9606fff0322b086b3
NatalijaMaulina <112900467+NatalijaMaulina@users.noreply.github.com>
2022-09-28 20:17:01 +0300

    summa

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello my name is Mrs Maulina");
+
+        int x = 10;
+        int z = 5;
+        int rez = x+z;
+        System.out.println(rez);
     }
 
 
