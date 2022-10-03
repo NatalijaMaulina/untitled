fcbcfadf9659afae9fd2fd15d4d3682da7ee2634
NatalijaMaulina <112900467+NatalijaMaulina@users.noreply.github.com>
2022-10-03 17:32:37 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello my name is Mrs Maulina");
+        System.out.println("Hello my name is Mrs Maulina, I am cat person");
 
         int x = 10;
         int z = 5;
         int rez = x+z;
         System.out.println(rez);
+
     }
 
 
