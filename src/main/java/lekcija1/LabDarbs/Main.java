721545640f9ac6937bdfad3f5d31d01f5edc529f
NatalijaMaulina <112900467+NatalijaMaulina@users.noreply.github.com>
2022-10-04 21:31:49 +0300

    Update Main.java

--- src/main/java/lekcija1/LabDarbs/Main.java
+++ src/main/java/lekcija1/LabDarbs/Main.java
@@ -1,10 +1,20 @@
 package lekcija1.LabDarbs;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-         int vecums = 31;
-        System.out.println("Mans vecums ir - "+ vecums);
-
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadiet luksofora krāsu");
+        String luksofors = scanner.nextLine();
+        if (luksofors.equals("red")) {
+            System.out.println("STAVI");
+        } else if (luksofors.equals("green")) {
+            System.out.println("Ej");
+        } else if (luksofors.equals("orange")) {
+            System.out.println("Gaidi");
+        } else {
+            System.out.println("Nedeg neviena gaisma");
+        }
     }
 }
