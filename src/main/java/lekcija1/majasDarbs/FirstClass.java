04944221707d4ed5f0695d83ad219cb6bcaadc3a
NatalijaMaulina <112900467+NatalijaMaulina@users.noreply.github.com>
2022-10-09 18:02:48 +0300

    Create FirstClass.java

---
+++ src/main/java/lekcija1/majasDarbs/FirstClass.java
@@ -0,0 +1,32 @@
+package lekcija1.majasDarbs;
+
+import java.util.Scanner;
+
+public class FirstClass {
+    public static void main(String[] args) {
+//        " Jaunzēlandes platība ir  "+ platiba " kvadrātkilometri" + " un tās galvaspilsēta ir" + pilseta " Iedzīvotāji pārsvarā runa " + valoda "valodās. Valsts valūta ir - NZ " + valuta);
+//        " - ir valsts Okeānijā, kurā dzīvo "
+        String valsts = "Jaunzēlande";
+        int iedzivotaji = 5132690;
+        double platiba =268.7;
+        String pilseta = "Vellingtona";
+        String valoda = "Angļu un maori";
+        char valuta = '$';
+        System.out.println(valsts);
+        System.out.println(" - ir valsts Okeānijā, kurā dzīvo");
+
+        int x = 20;
+        int y = 5 ;
+        int summa =x+y;
+        int starpiba = x-y;
+        int reizinajums = x*y;
+        int dalijums = x/y;
+        System.out.println(" Skaitļu summa ir :" + summa);
+        System.out.println(" Skaitļu starpība ir " + starpiba);
+        System.out.println(" Skaitļu reizinājums ir " + reizinajums);
+        System.out.println(" Skaitļu dalījums ir " + dalijums);
+
+
+    }
+}
+
