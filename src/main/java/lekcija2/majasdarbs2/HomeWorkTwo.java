9a2494c05e53804fc567f95c62412ea73274fe6e
NatalijaMaulina <112900467+NatalijaMaulina@users.noreply.github.com>
2022-10-15 13:25:52 +0300

    md

---
+++ src/main/java/lekcija2/majasdarbs2/HomeWorkTwo.java
@@ -0,0 +1,101 @@
+package lekcija2.majasdarbs2;
+
+import java.util.Scanner;
+
+public class HomeWorkTwo {
+    public static void main(String[] args) {
+        printCard();
+        printCard();
+        printCard();
+        printBusinessCard2("Jānis","Bērziņš", "65463252","2005");
+        printBusinessCard2("Līga", "Liepiņa", "56478239","1985");
+
+    }
+public static int izteiksmes (){
+        int x = 6;
+        System.out.println(x>0);
+        System.out.println(x<0);
+        System.out.println(x>5 && x=10 || x==10);
+        System.out.println(x>5 && x!=5 && x<10);
+        System.out.println(x=0||x=10);
+        System.out.println(x*x>10);
+    }
+    public static int month (){
+        Scanner ievade= new Scanner(System.in);
+        System.out.println("Ievadiet mēneša numuru ");
+        int month = ievade.nextInt();
+        switch (month) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("Fenruary");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+        }
+    }
+////
+////    public static int maxNumber (){
+////        Scanner ievade2= new Scanner(System.in);
+////        System.out.println("Ievadiet trīs veselus skaitļus ");
+////        int x = ievade2.nextInt();
+////        int y = ievade2.nextInt();
+////        int z = ievade2.nextInt();
+////        int max = Math.max(x;y);
+//
+//    }
+    public static void luksofors() {
+        Scanner ievade3 = new Scanner(System.in);
+        System.out.println("Ievadiet luksofora krāsu");
+        String luksofors = ievade3.nextLine();
+        if (luksofors.equals("red")) {
+            System.out.println("STAVI");
+        } else if (luksofors.equals("green")) {
+            System.out.println("Ej");
+        } else if (luksofors.equals("orange")) {
+            System.out.println("Gaidi");
+        } else {
+            System.out.println("Nedeg neviena gaisma");
+        }
+    }
+    public static void printCard(){
+        System.out.println("NATĀLIJA MAUĻINA");
+        System.out.println( "12345678");
+        System.out.println("1991");
+    }
+    public static void printBusinessCard2 (String vards, String uzvards, long numurs,int gads){
+        System.out.println(vards + " " + uzvards);
+        System.out.println(numurs);
+        System.out.println(gads);
+    }
+
+}
--- src/main/java/lekcija3/labDarbs/Main.java
+++ src/main/java/lekcija3/labDarbs/Main.java
@@ -6,48 +6,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//    printCard();
-//    printCard();
-//    int laukums = aprekinaLaukumu(3,10);
-//    printCard2("Natālija", "Mauļina", "Olaine", 31);
-//        int[] menesaTerini= {100,40,23,44,55,254} ;
-//        System.out.println("Masīva lielums ir" + menesaTerini.length);
-//        String[]produktuSaraksts = {"Piens", "Maize", "Siers"};
-//        System.out.println([2]);
-//        produktuSaraksts[2]="Biezpiens";
-
-        int[] menesi = new int[12];
-        menesi[0]=1;
-        char[] vards = {'N','A','T', 'Ā', 'L','I', 'J', 'A'};
-        for(char mansVards: vards){
-            System.out.println(mansVards);
-
-    }
-    public static int aprekinaLaukumu (int platums, int garums){
-        return garums*platums;
-    }
-    public static void printCard(){
-        System.out.println("**********");
-        System.out.println("NATALIJA MAUĻINA");
-        System.out.println( "OLAINE");
-        System.out.println("$$$$$$$$$$$");
-    }
-    public static void whileCikls(){
-        int x=5;
-        while(x>0){
-            System.out.println("x nav nulle, x = " + x );
-            x--;
-        }
+    printCard();
+    printCard();
+    int laukums = aprekinaLaukumu(3,10);
         int i = 10;
         while(i<50){
             System.out.println( "i ir " + i);
             i++;
-        }
-        String[]cars= {"BMW", "Audi", "Mercedes", "Alfa Romeo"};
-        int z=0;
-        while (z<4){
-            System.out.println(cars[z]);
-            z++;
         }
         Scanner ievade= new Scanner(System.in);
         System.out.println("Ievadi skaitli  ");
@@ -56,10 +21,9 @@ public class Main {
         while(number>0){
             summa=summa+number;
             System.out.println( "Ievadi skaitli ");
-            number=ievade.nextInt()
+            number=ievade.nextInt();
         }
         System.out.println("Summa ir: " + summa);
-
         String[] kartis= { "Pīķa dūzis", "Ercena kalps", "Kreiča dāma"};
         int i=0;
         while( i< kartis.length){
@@ -67,33 +31,65 @@ public class Main {
             i++;
 
         }
-        public static void dowhileCikls() {
-                int q = 1;
-                do {
-                    System.out.println("TEST TEST TEST");
-                    q++;
-                } while (q < 10) {
 
-                }
+    }
+    public static int aprekinaLaukumu (int platums, int garums){
+        return garums*platums;
+    }
+    public static int masivi() {
+        int[] menesaTerini = {100, 40, 23, 44, 55, 254};
+        System.out.println("Masīva lielums ir" + menesaTerini.length);
+        String[] produktuSaraksts = {"Piens", "Maize", "Siers"};
+        produktuSaraksts[2] = "Biezpiens";
+    }
+    public static char name() {
+        char[] vards = {'N', 'A', 'T', 'Ā', 'L', 'I', 'J', 'A'};
+        for (char mansVards : vards) {
+            System.out.println(mansVards);
+        }
+    }
+    public static void printCard(){
+        System.out.println("**********");
+        System.out.println("NATALIJA MAUĻINA");
+        System.out.println( "OLAINE");
+        System.out.println("$$$$$$$$$$$");
+    }
+    public static void whileCikls() {
+        int x = 5;
+        while (x > 0) {
+            System.out.println("x nav nulle, x = " + x);
+            x--;
+        }
+    }
+    public static String cars () {
+        String[] cars = {"BMW", "Audi", "Mercedes", "Alfa Romeo"};
+        int z = 0;
+        while (z < 4) {
+            System.out.println(cars[z]);
+            z++;
+        }
+    }
+
+//        public static void dowhileCikls() {
+//                int q = 1;
+//                do {
+//                    System.out.println("TEST TEST TEST");
+//                    q++;
+//                } while (q < 10) {
+//
+//            }
+//                }
                 public static void forCikls () {
                     String[] kartis2 = {"Pīķa desmitnieks", "Kārava karalis", "Džokers"};
                     for (int j = 0; j < kartis2.length; j++) {
                         System.out.println(kartis2[j]);
                     }
-                    long[] telNumuri = {2535535635, 5222222, 1111666, 33377777}
+                    long[] telNumuri = {2535635, 5222222, 1111666, 33377777};
                     for (long numurs : telNumuri) {
                         System.out.println(numurs);
                     }
 
                 }
-//                public static void printCard2 (String vards, String uzvards, String pilseta,int vecums){
-//                    System.out.println(vards + " " + uzvards);
-//                    System.out.println(pilseta);
-//                    System.out.println(vecums);
-//                    System.out.println("******");
-//                }
-
-            }
 
 
 }
